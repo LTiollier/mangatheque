198c8caa519a877d244f29859521cdc4734969f8
Léo TIOLLIER <ltiollier30@gmail.com>
2026-02-22 21:22:18 +0100

    :card_index: Create mangas and user_manga migrations

---
+++ laravel-api/database/migrations/2026_02_22_200730_create_mangas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mangas', function (Blueprint $table) {
+            $table->id();
+            $table->string('api_id')->unique()->nullable();
+            $table->string('isbn')->unique()->nullable();
+            $table->string('title');
+            $table->json('authors')->nullable();
+            $table->text('description')->nullable();
+            $table->string('published_date')->nullable();
+            $table->integer('page_count')->nullable();
+            $table->string('cover_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mangas');
+    }
+};
