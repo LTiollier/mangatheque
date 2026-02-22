66acab60397ce839bd7bdbd6973702ac743d0bbe
Léo TIOLLIER <ltiollier30@gmail.com>
2026-02-22 16:23:40 +0100

    :sparkles: Implement Logout logic with DDD Action

---
+++ laravel-api/app/User/Application/Actions/LogoutAction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\User\Application\Actions;
+
+use App\User\Domain\Models\User;
+use App\User\Domain\Repositories\UserRepositoryInterface;
+
+class LogoutAction
+{
+    public function __construct(
+        private readonly UserRepositoryInterface $userRepository
+    ) {
+    }
+
+    public function execute(User $user): void
+    {
+        $this->userRepository->revokeTokens($user);
+    }
+}
