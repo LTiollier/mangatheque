ff3cc3b5544a41a328117be7dc03023d1489c4dc
Léo TIOLLIER <ltiollier30@gmail.com>
2026-02-22 16:21:10 +0100

    :sparkles: Add login API endpoint

--- laravel-api/app/Http/Api/Controllers/AuthController.php
+++ laravel-api/app/Http/Api/Controllers/AuthController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Api\Controllers;
 
+use App\Http\Api\Requests\LoginRequest;
 use App\Http\Api\Requests\RegisterRequest;
 use App\Http\Api\Resources\UserResource;
+use App\User\Application\Actions\LoginAction;
 use App\User\Application\Actions\RegisterUserAction;
+use App\User\Application\DTOs\LoginDTO;
 use App\User\Application\DTOs\RegisterUserDTO;
+use App\User\Domain\Exceptions\InvalidCredentialsException;
+use Illuminate\Http\JsonResponse;
 
 class AuthController
 {
-    public function register(RegisterRequest $request, RegisterUserAction $action): \Illuminate\Http\JsonResponse
+    public function register(RegisterRequest $request, RegisterUserAction $action): JsonResponse
     {
         $dto = new RegisterUserDTO(
             name: $request->validated('name'),
@@ -22,5 +27,26 @@ class AuthController
         return (new UserResource($user))
             ->response()
             ->setStatusCode(201);
+    }
+
+    public function login(LoginRequest $request, LoginAction $action): JsonResponse
+    {
+        try {
+            $dto = new LoginDTO(
+                email: $request->validated('email'),
+                password: $request->validated('password'),
+            );
+
+            $result = $action->execute($dto);
+
+            return response()->json([
+                'user' => new UserResource($result['user']),
+                'token' => $result['token'],
+            ]);
+        } catch (InvalidCredentialsException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 401);
+        }
     }
 }
