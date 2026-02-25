83ff412220f46eb2b54af4387f0b9cb199fd8797
Léo TIOLLIER <ltiollier30@gmail.com>
2026-02-25 09:35:25 +0100

    :wrench: Add placeholder and google user content hostnames to next.config.ts

--- pwa-client/next.config.ts
+++ pwa-client/next.config.ts
@@ -49,6 +49,18 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
