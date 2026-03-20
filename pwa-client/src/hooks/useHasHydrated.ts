9707af94b7bd3a9ec2b3a28f9edf1dfa6224cc20
Léo TIOLLIER <ltiollier30@gmail.com>
2026-03-20 13:35:46 +0100

    :fire: Remove frontend v1

--- pwa-client/src/hooks/useHasHydrated.ts
+++
@@ -1,9 +0,0 @@
-import { useSyncExternalStore } from 'react';
-
-const subscribe = () => () => { };
-const getSnapshot = () => true;
-const getServerSnapshot = () => false;
-
-export function useHasHydrated() {
-    return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
-}
--- pwa-client/src/types/auth.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-    username: string | null;
-    is_public: boolean;
-}
